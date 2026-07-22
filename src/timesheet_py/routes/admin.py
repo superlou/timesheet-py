a23de1b46a74b0369914cb6bf9ffbd4235621565
superlou <lousimons@gmail.com>
2026-07-21 20:48:45 -0400

    Made projects and activities into lists

--- src/timesheet_py/routes/admin.py
+++ src/timesheet_py/routes/admin.py
@@ -83,20 +83,31 @@ class ProjectRow:
     @ui.refreshable_method
     def display(self):
         if self.editing:
-            code_input = ui.input(value=self.project.code).props("dense")
-            name_input = ui.input(value=self.project.name).props("dense")
-            open_input = ui.checkbox(value=self.project.open).props("dense")
-            ui.button(
-                icon="save",
-                on_click=lambda: self.save(
-                    code_input.value, name_input.value, open_input.value
-                ),
-            )
+            with ui.item_section():
+                code_input = ui.input(value=self.project.code).props("dense")
+            with ui.item_section():
+                name_input = ui.input(value=self.project.name).props("dense")
+            with ui.item_section():
+                open_input = ui.checkbox(value=self.project.open).props("dense")
+            with ui.item_section():
+                ui.button(
+                    icon="save",
+                    on_click=lambda: self.save(
+                        code_input.value, name_input.value, open_input.value
+                    ),
+                )
         else:
-            ui.label(self.project.code)
-            ui.label(self.project.name)
-            ui.label(str(self.project.open))
-            ui.button(icon="edit", on_click=self.edit)
+            with ui.item_section():
+                ui.label(self.project.code)
+
+            with ui.item_section():
+                ui.label(self.project.name)
+
+            with ui.item_section():
+                ui.label("open" if self.project.open else "closed")
+
+            with ui.item_section():
+                ui.button(icon="edit", on_click=self.edit).props("flat dense")
 
     def edit(self):
         self.editing = True
@@ -114,24 +125,30 @@ class ProjectRow:
 @router.page("/projects")
 async def projects(user: CurrentUser):
     header(user)
-    ui.label("Projects")
 
     @ui.refreshable
     async def projects_list():
         projects = await Project.all()
-        with ui.grid(columns=4):
-            ui.label("Code")
-            ui.label("Name")
-            ui.label("Open")
-            ui.label()
+        with ui.list().props("bordered separator").classes("w-full"):
+            ui.item_label("Projects").props("header").classes("text-bold")
+            ui.separator()
 
             for project in projects:
-                ProjectRow(project).display()
+                with ui.item():
+                    ProjectRow(project).display()
 
-            new_project_code = ui.input("Code").props("dense")
-            new_project_name = ui.input("Name").props("dense")
-            new_project_open = ui.checkbox().props("dense")
-            ui.button(icon="add", on_click=lambda: create_project())
+            with ui.item():
+                with ui.item_section():
+                    new_project_code = ui.input("Code").props("dense")
+                with ui.item_section():
+                    new_project_name = ui.input("Name").props("dense")
+                with ui.item_section():
+                    new_project_open = ui.checkbox().props("dense")
+
+                with ui.item_section():
+                    ui.button(icon="add", on_click=lambda: create_project()).props(
+                        "flat dense"
+                    )
 
             async def create_project():
                 await Project.create(
@@ -146,37 +163,90 @@ async def projects(user: CurrentUser):
     await projects_list()
 
 
+class ActivityRow:
+    def __init__(self, activity: Activity, editing: bool = False):
+        self.activity = activity
+        self.editing = editing
+
+    @ui.refreshable_method
+    def display(self):
+        if self.editing:
+            with ui.item_section():
+                code_input = ui.input(value=self.activity.code).props("dense")
+            with ui.item_section():
+                name_input = ui.input(value=self.activity.name).props("dense")
+            # with ui.item_section():
+            #     open_input = ui.checkbox(value=self.activity.open).props("dense")
+            with ui.item_section():
+                ui.button(
+                    icon="save",
+                    on_click=lambda: self.save(
+                        code_input.value,
+                        name_input.value,  # , open_input.value
+                    ),
+                )
+        else:
+            with ui.item_section():
+                ui.label(self.activity.code)
+
+            with ui.item_section():
+                ui.label(self.activity.name)
+
+            # with ui.item_section():
+            #     ui.label("open" if self.activity.open else "closed")
+
+            with ui.item_section():
+                ui.button(icon="edit", on_click=self.edit).props("flat dense")
+
+    def edit(self):
+        self.editing = True
+        self.display.refresh()
+
+    async def save(self, code: str, name: str):  # , open: bool):
+        self.activity.code = code
+        self.activity.name = name
+        self.activity.open = open
+        await self.activity.save()
+        self.editing = False
+        self.display.refresh()
+
+
 @router.page("/activities")
 async def activities(user: CurrentUser):
     header(user)
-    ui.label("Activities")
 
     @ui.refreshable
     async def activities_list():
         activities = await Activity.all()
-        with ui.grid(columns=2):
-            ui.label("Code")
-            ui.label("Name")
+        with ui.list().props("bordered separator").classes("w-full"):
+            ui.item_label("Activities").props("header").classes("text-bold")
+            ui.separator()
 
             for activity in activities:
-                ui.label(activity.code)
-                ui.label(activity.name)
+                with ui.item():
+                    ActivityRow(activity).display()
 
-    async def create_activity():
-        await Activity.create(
-            code=new_activity_code.value, name=new_activity_name.value
-        )
-        new_activity_code.value = ""
-        new_activity_name.value = ""
-        activities_list.refresh()
+            with ui.item():
+                with ui.item_section():
+                    new_activity_code = ui.input("Code")
+
+                with ui.item_section():
+                    new_activity_name = ui.input("Name")
+
+                with ui.item_section():
+                    ui.button(icon="add", on_click=lambda: create_activity()).props(
+                        "flat dense"
+                    )
+
+        async def create_activity():
+            await Activity.create(
+                code=new_activity_code.value, name=new_activity_name.value
+            )
+            new_activity_code.value = ""
+            new_activity_name.value = ""
+            activities_list.refresh()
 
     await activities_list()
-
-    with ui.row().classes("items-center"):
-        ui.label("New activity")
-        new_activity_code = ui.input("Code")
-        new_activity_name = ui.input("Name")
-        ui.button("Create", on_click=create_activity)
 
 
 @router.page("/users")
