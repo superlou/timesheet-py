03798ada102c7cea697456a9e933355bdce5f8fa
superlou <lousimons@gmail.com>
2026-07-24 22:23:45 -0400

    Formatted users as list

--- src/timesheet_py/routes/admin/users.py
+++ src/timesheet_py/routes/admin/users.py
@@ -25,39 +25,50 @@ async def users(user: CurrentUser):
     async def user_table():
         users = await User.all().prefetch_related("approvers")
 
-        with ui.grid(columns=6).classes("items-center"):
-            ui.label("Name")
-            ui.label("Email")
-            ui.label("Code")
-            ui.label("Admin")
-            ui.label("API Access")
-            ui.label("Approver")
+        with ui.list().props("bordered separator").classes("items-center w-full"):
+            with ui.item():
+                with ui.item_section():
+                    ui.item_label("User").props("header").classes("text-bold")
+                with ui.item_section():
+                    ui.item_label("Admin").props("header").classes("text-bold")
+                with ui.item_section():
+                    ui.item_label("API Access").props("header").classes("text-bold")
+                with ui.item_section():
+                    ui.item_label("Approver").props("header").classes("text-bold")
 
             for user in users:
-                ui.label(user.name)
-                ui.label(user.email)
-                ui.label(user.code)
-                ui.checkbox(
-                    value=user.admin,
-                    on_change=lambda evt, user=user: update_user_admin(user, evt.value),
-                )
-                ui.checkbox(
-                    value=user.api_access,
-                    on_change=lambda evt, user=user: update_user_api_access(
-                        user, evt.value
-                    ),
-                )
-                ui.select(
-                    {user: user.name for user in users},
-                    value=[approver for approver in user.approvers],
-                    multiple=True,
-                    on_change=lambda evt, user=user: update_user_approvers(
-                        user, evt.value
-                    ),
-                )
+                with ui.item():
+                    with ui.item_section():
+                        ui.label(user.name)
+                        ui.label(user.email).classes("text-caption")
+                        ui.label(user.code).classes("text-caption")
+                    with ui.item_section():
+                        ui.checkbox(
+                            value=user.admin,
+                            on_change=lambda evt, user=user: update_user_admin(
+                                user, evt.value
+                            ),
+                        )
+                    with ui.item_section():
+                        ui.checkbox(
+                            value=user.api_access,
+                            on_change=lambda evt, user=user: update_user_api_access(
+                                user, evt.value
+                            ),
+                        )
+                    with ui.item_section():
+                        ui.select(
+                            {user: user.name for user in users},
+                            value=[approver for approver in user.approvers],
+                            multiple=True,
+                            on_change=lambda evt, user=user: update_user_approvers(
+                                user, evt.value
+                            ),
+                        )
 
-    with ui.row():
+    with ui.row().classes("w-full"):
         with ui.column().classes("col-2"):
             admin_menu()
         with ui.column().classes("col-8"):
+            ui.label("Users")
             await user_table()
