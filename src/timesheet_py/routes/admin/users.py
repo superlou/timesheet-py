dc13f5798e755e031e493d5a1e3dec27b2a6cb34
superlou <lousimons@gmail.com>
2026-08-02 14:06:46 -0400

    Added user deletion to fix #20

--- src/timesheet_py/routes/admin/users.py
+++ src/timesheet_py/routes/admin/users.py
@@ -19,6 +19,30 @@ async def users(user: CurrentUser):
         await user.approvers.clear()
         await user.approvers.add(*approvers)
 
+    async def delete_user(user: User):
+        await user.delete()
+        user_table.refresh()
+
+    dialog = ui.dialog()
+
+    def open_delete_user_dialog(user: User):
+        dialog.clear()
+        with dialog, ui.card():
+            with ui.card_section():
+                ui.label(f"Delete {user.name}?").classes("text-h6")
+
+            with ui.card_section():
+                ui.label(
+                    f"This will delete all data associated with the user {user.email}."
+                )
+
+            with ui.card_section():
+                with ui.row():
+                    ui.button("Cancel", on_click=dialog.close)
+                    ui.button("Delete", on_click=lambda: delete_user(user))
+
+        dialog.open()
+
     @ui.refreshable
     async def user_table():
         users = await User.all().prefetch_related("approvers")
@@ -33,6 +57,8 @@ async def users(user: CurrentUser):
                     ui.item_label("API Access").props("header").classes("text-bold")
                 with ui.item_section():
                     ui.item_label("Approver").props("header").classes("text-bold")
+                with ui.item_section():
+                    pass
 
             for user in users:
                 with ui.item():
@@ -63,6 +89,11 @@ async def users(user: CurrentUser):
                                 user, evt.value
                             ),
                         )
+                    with ui.item_section():
+                        ui.button(
+                            icon="delete",
+                            on_click=lambda user=user: open_delete_user_dialog(user),
+                        ).props("flat")
 
     ui.label("Users")
     await user_table()
